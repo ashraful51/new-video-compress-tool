946184c01a6e0711d04dcaad0bbc062649e8d379
Ashraful Alam <58485838+ashraful51@users.noreply.github.com>
2024-08-02 10:59:16 +0600

    Verify ffmpeg-static installation and path

--- check-ffmpeg.js
+++ check-ffmpeg.js
@@ -1,14 +1,14 @@
+const ffmpegPath = require('ffmpeg-static');
+console.log('Checking ffmpeg path:', ffmpegPath);
+
 const fs = require('fs');
 const path = require('path');
-const ffmpegPath = require('ffmpeg-static');
-
-console.log('Checking ffmpeg path:', ffmpegPath);
 
 fs.access(ffmpegPath, fs.constants.F_OK, (err) => {
   if (err) {
-    console.error('Cannot find ffmpeg');
+    console.error('Cannot find ffmpeg:', err);
     process.exit(1);
   } else {
-    console.log('ffmpeg is installed correctly');
+    console.log('ffmpeg is installed correctly at:', ffmpegPath);
   }
 });
